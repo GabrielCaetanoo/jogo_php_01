f6c1fdbbe8026265a985859c4bbc6dfc15251b9e
Gabriel Caetano <gabrielc0202@hotmail.com>
2025-01-20 22:27:21 -0300

    1/2

--- scripts/start.php
+++ scripts/start.php
@@ -1,3 +1,85 @@
 <?php 
+// Check if there was a post to initialize the game 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    //Get total questions
+    $total_questions = intval($_POST['text_total_questions']) ?? 10;
+
+    prepare_game($total_questions);
+
+    // redirect to game
+    header('Location: index.php?route=game');
+    exit;
+}
+
+function prepare_game($total_questions){
+    // lets prepare all game here... 
+    global $capitals;
+
+    // get random items
+    $ids = [];
+    while(count($ids) < $total_questions){
+        $id = rand(0, count($capitals) - 1);
+        if(!in_array($id, $ids)){
+            $ids[] = $id;
+        }
+    }
+    // define first data for $questions
+    $questions = [];
+    foreach($ids as $id){
+
+        //get correct answer and two incorrect answers
+        $answers = [];
+        $answers[] = $id;
+        while(count($answers) < 3){
+            $tmp = rand(0, count($capitals) - 1);
+            if(!in_array($tmp, $answers)){
+                $answers[] = $tmp;
+            }
+        }
+
+        // add 3 possible answers to the question
+        shuffle($answers);
+
+        // add question to $questions
+        $questions[] = [
+            'question' => $capitals[$id][0],
+            'correct_answer' => $id,
+            'answers' => $answers
+        ];
+    }
+
+    echo '<pre>';
+    print_r($questions);
+    die();
+}
 
 ?>
+
+<div class="container mt-5">
+    <div class="row justify-contet-center">
+        <div class="div col-6">
+            <div class="card p5">
+                <div class="row">
+                    <div class="col text-center">
+                        <h3>Jogo das Capitais</h3>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-4">
+                        <form action="index.php?route=start" method="post">
+                            <div class="mb-3">
+                                <label for="text_total_questions" class="form-label">Número de questões:</label>
+                                <input type="number" name="text_total_questions" class="form-control form-control-lg text-center" min="3" max="20" value="10" >
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-success w-100">Iniciar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
